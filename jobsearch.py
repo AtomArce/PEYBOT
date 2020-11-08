baa10a2fd72aa155413e44dee35abcd34864c45c
AtomArce <66486660+AtomArce@users.noreply.github.com>
2020-11-08 01:19:59 -0500

    Add files via upload

--- jobsearch.py
+++ jobsearch.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
-from bs4 import BeautifulSoup
-import requests
+import re
+from docx import Document
+from docx2pdf import convert
+
 
 browser = webdriver.Chrome(executable_path= "d:\chromedriver.exe")
 
@@ -41,13 +43,108 @@ browser.find_element_by_xpath('//*[@id="dashboard"]/div[2]/div[1]/a').click();
 
 
 #========================================= filtering jobs===========================#
+
+#==========================collect all job ids===========================#
+
 #list of all job_ids
 job_ids = []
-stable = 0
-while (stable != 1): #iterate through all pages until page does not change
-    col_1 = browser.find_elements_by_xpath('//*[@id="postingsTable"]/tbody//td[1]')  #selenium elements
-    for id in col_1:
-        job_ids.append(id.text)
-    #change the page
 
-    return True
+col_1 = browser.find_elements_by_xpath('//*[@id="postingsTable"]/tbody//td[1]')  #selenium elements
+for id in col_1:
+    job_ids.append(id.text)  #ids are string type
+
+#return to overview
+browser.find_element_by_xpath('//*[@id="mainContentDiv"]/div[1]/div/div[2]/ul/li/a').click();
+
+########################Cover Letter Stuff######################
+def docx_replace_regex(doc_obj, regex , replace):
+
+    for p in doc_obj.paragraphs:
+        if regex.search(p.text):
+            inline = p.runs
+            # Loop added to work with runs (strings with same style)
+            for i in range(len(inline)):
+                if regex.search(inline[i].text):
+                    text = regex.sub(replace, inline[i].text)
+                    inline[i].text = text
+
+    for table in doc_obj.tables:
+        for row in table.rows:
+            for cell in row.cells:
+                docx_replace_regex(cell, regex , replace)
+
+def cover_letter_gen():
+    regex1 = re.compile(r"job_name")
+    regex2 = re.compile(r"company_name")
+    replace1 = job_name
+    replace2 = company_name
+    doc_name = "coverletter.docx"
+    doc = Document(doc_name)
+    docx_replace_regex(doc, regex1, replace1)
+    docx_replace_regex(doc, regex2, replace2)
+    file_name = "CoverLetter-"+ id
+    doc.save(file_name + ".docx")
+
+    #Generates pdf file
+    convert(file_name + ".docx")
+    convert(file_name + ".docx", file_name + ".pdf")
+#########################################################################
+
+applied = []
+external = []
+
+#=========================== loop through job ids and apply if possible===========================#
+for id in job_ids:
+    #generate cover letter name & cover letter file
+
+    # go to search bar for job id
+    element = browser.find_element_by_xpath('// *[ @ id = "searchByPostingNumberForm"] / input[2]')
+    browser.execute_script("arguments[0].scrollIntoView();", element)
+    element.click();
+
+    # #Click on apply button
+    # browser.find_element_by_xpath('//*[@id="applyButton"]').click()
+
+    # input id into bar
+    browser.find_element_by_xpath('//*[@id="searchByPostingNumberForm"]/input[2]').send_keys(id)
+    # click on search
+    browser.find_element_by_xpath('//*[@id="searchByPostingNumberForm"]/a').click()
+
+    job_name = browser.find_element_by_xpath('//*[@id="mainContentDiv"]/div[1]/div/div[2]/h1').text
+    company_name = browser.find_element_by_xpath('//*[@id="postingDiv"]/table[3]/tbody/tr[1]/td[2]').text
+
+    #click on apply for this position
+    browser.find_element_by_xpath('//*[@id="applyButton"]').click()
+
+
+    try: #check if I intend to apply exist->external
+        browser.find_element_by_xpath('//*[@id="mainContentDiv"]/div[2]/div/div/div/div[2]/div/div/form/input[3]')
+        external.append(id)  #add to external id list
+        browser.find_element_by_xpath('//*[@id="mainContentDiv"]/div[1]/div/div[3]/ul/li/a').click()
+
+
+    except: #apply case
+        #click on upload document
+        browser.find_element_by_xpath('//*[@id="mainContentDiv"]/div[2]/div/div/div/div[2]/div/div/div[2]/div[1]/a').click();
+        #input cover letter name
+        browser.find_element_by_xpath('//*[@id="fileUploadForm"]/div[1]/div[2]/input').send_keys(file_name)
+
+        # click on document type
+        browser.find_element_by_xpath('//*[@id="fileUploadForm"]/div[2]/div[2]/select').click()
+        # click on cover letter option
+        browser.find_element_by_xpath('//*[@id="fileUploadForm"]/div[2]/div[2]/select/option[2]').click()
+        #click on choose file
+
+        cover_letter_gen()
+
+        browser.find_element_by_xpath('//*[@id="fileUploadForm"]/div[3]/div[2]/input').send_keys(r"D:\Users\Atom\PycharmProjects\PEYBOT\CoverLetter-" + id + ".pdf")
+        #click on upload
+        browser.find_element_by_xpath('//*[@id="mainContentDiv"]/div[2]/div/div/div/div/div/a[1]').click()
+
+        break
+
+
+
+
+
+
